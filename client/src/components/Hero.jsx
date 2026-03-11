f91ad09a5626b8f408cf6e69dcdcc04dd59d8e1f
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-11 19:34:49 +0500

    commit 8

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,13 @@
+import BookingForm from "./BookingForm"
+
 const Hero = () => {
   return (
     <div className="hero-container">
-
+        <p className="tagline">Where Great Journeys Begin</p>
+        <h1 className="title font-playfair">Your Gateway to Premium Hotels</h1>
+        <p className="subtitle">Explore top destinations, discover luxury accommodations, and book your perfect stay effortlessly.</p>
+        
+        <BookingForm />
     </div>
   )
 }
