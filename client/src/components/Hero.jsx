5a0103f036fdc2d97a19dc4e91ac800a253d8de4
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-11 17:38:57 +0500

    commit 7

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url('')]">
+    <div className="hero-container">
 
     </div>
   )
