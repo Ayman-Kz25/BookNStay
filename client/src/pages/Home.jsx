83af8cac4d6252e44adb407de4ef0f5e84748d33
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-12 19:09:34 +0500

    commit 17

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import ExclusiveOffers from "../components/ExclusiveOffers"
 import FeaturedDestination from "../components/FeaturedDestination"
+import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import Newsletter from "../components/Newsletter"
 import Testimonial from "../components/Testimonial"
