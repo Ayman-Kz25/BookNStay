710da360a97fc6cb979f1a5fdc9dcd0dbfbed814
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-14 23:46:54 +0500

    commit 41

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -3,29 +3,30 @@ import SectionTitle from "./SectionTitle";
 
 const Newsletter = () => {
   return (
-    <div className="flex flex-col items-center max-w-5xl lg:w-full rounded-2xl px-4 py-12 md:py-16 mx-2 lg:mx-auto my-30 bg-[var(--primary)] text-white">
-
+    <section className="newsletter-section">
       <SectionTitle
         title="Stay Inspired"
         subtitle="Join our newsletter and be the first to discover new updates, exclusive offers, and inspiration."
       />
 
-      <div className="flex flex-col md:flex-row items-center justify-center gap-4 mt-6">
+      <div className="newsletter-form">
         <input
           type="text"
-          className="bg-[var(--secondary)]/20 px-4 py-2.5 border-2 border-[var(--secondary)]/80 text-[var(--text-accent)] rounded-lg outline-none max-w-66 w-full placeholder:text-white/70"
+          className="newsletter-input"
           placeholder="Enter your email"
         />
-        <button className="flex items-center justify-center gap-2 group bg-[var(--secondary)] text-white px-4 md:px-7 py-2.5 rounded-lg active:scale-95 transition-all cursor-pointer">
+        <button className="newsletter-btn group">
           Subscribe
-          <MoveRight size={16} className=" group-hover:translate-x-1 transition-all" />
+          <MoveRight size={16} className="newsletter-btn-icon" />
         </button>
       </div>
-      <p className="text-gray-500 mt-6 text-xs text-center">
+
+      <p className="newsletter-disclaimer">
         By subscribing, you agree to our Privacy Policy and consent to receive
         updates.
       </p>
-    </div>
+    </section>
   );
 };
+
 export default Newsletter;
