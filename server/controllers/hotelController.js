77ab1054df4e01b1324442566a330d4906e6b0a4
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-26 19:44:24 +0500

    commit 75

---
+++ server/controllers/hotelController.js
@@ -0,0 +1,17 @@
+import Hotel from "../models/Hotel.js";
+import User from "../models/User.js";
+
+export const registerHotel = async (req, res) => {
+    try {
+        const {name, address, contact, city} = req.body;
+        const owner = req.user._id;
+
+        //check if the user is already registered
+        const hotel = await Hotel.findOne({owner});
+        if(hotel){
+            return res.json({success: false, message})
+        }
+    } catch (error) {
+        
+    }
+}
