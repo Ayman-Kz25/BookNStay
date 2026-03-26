6b65be986b371a1204c152ce0c9a2485ec1a2b1d
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-26 11:34:39 +0500

    commit 70

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+const {Schema} = mongoose;
 
-const userSchema = mongoose.Schema({
+const userSchema = new Schema({
     id: {type: String, required: true},
     username: {type: String, required: true},
     email: {type: String, required: true},
--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
 const app = express();
 
 app.use(cors()); //Enable Cross-Origin Resource Sharing
+
 //Middleware
 app.use(express.json());
 app.use(clerkMiddleware());
+
+// API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks)
 
 app.get("/", (req, res) => res.send("API is working"));
 
