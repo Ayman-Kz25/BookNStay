7ca0d3407d65225357a04797b976b22eb54b240e
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-27 16:06:49 +0500

    commit 80

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes, useLocation } from "react-router-dom"
-import Navbar from "./components/Navbar"
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Rooms from "./pages/Rooms";
@@ -10,12 +10,13 @@ import Layout from "./pages/owner/Layout";
 import Dashboard from "./pages/owner/Dashboard";
 import AddRoom from "./pages/owner/AddRoom";
 import RoomsList from "./pages/owner/RoomsList";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-
-  const isOwnerPath = useLocation().pathname.includes('owner');
+  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
       {false && <Registration />}
       <div className="min-h-[70vh]">
@@ -25,14 +26,14 @@ const App = () => {
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-reservations" element={<Reservations />} />
           <Route path="/owner" element={<Layout />}>
-            <Route index element={<Dashboard />}/>
-            <Route path="add-room" element={<AddRoom />}/>
-            <Route path="rooms-list" element={<RoomsList />}/>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="rooms-list" element={<RoomsList />} />
           </Route>
         </Routes>
       </div>
       <Footer />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/react";
 import "./index.css";
 import App from "./App.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -13,7 +14,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <Router>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </Router>
   </ClerkProvider>,
 );
