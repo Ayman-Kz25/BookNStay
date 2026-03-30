42e624ffd11648b375c244c1d554d6887779014a
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-30 18:05:04 +0500

    commit 109

--- server/data/offers.js
+++ server/data/offers.js
@@ -4,7 +4,7 @@ export const offers = [
     discount: "30% OFF",
     city: "Hunza",
     description: "Enjoy a luxury weekend stay with breathtaking mountain views and premium resort amenities.",
-    img: "/images/offers/hunza.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774874707/rooms/hunza_whnqct.png",
     expiryDate: "30 June 2026"
   },
   {
@@ -12,7 +12,7 @@ export const offers = [
     discount: "20% OFF",
     city: "Karachi",
     description: "Perfect city getaway in Karachi's premium hotels with rooftop dining and sea breeze views.",
-    img: "/images/offers/karachi.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774874756/rooms/karachi_qdxgkr.jpg",
     expiryDate: "15 July 2026"
   },
   {
@@ -20,7 +20,7 @@ export const offers = [
     discount: "25% OFF",
     city: "Skardu",
     description: "Book luxury resorts in Skardu and experience glaciers, lakes, and mountain serenity.",
-    img: "/images/offers/skardu.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774868423/rooms/skardu_uuzhqv.jpg",
     expiryDate: "10 August 2026"
   },
   {
@@ -28,7 +28,7 @@ export const offers = [
     discount: "35% OFF",
     city: "Naran",
     description: "Relax beside the beautiful Saif-ul-Malook lake with discounted premium rooms.",
-    img: "/images/offers/naran.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774874924/rooms/470490112_mau2zq.jpg",
     expiryDate: "25 July 2026"
   },
   {
@@ -36,7 +36,7 @@ export const offers = [
     discount: "28% OFF",
     city: "Kaghan",
     description: "Escape to the peaceful valleys of Kaghan with cozy cabins and stunning landscapes.",
-    img: "/images/offers/kaghan.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875017/rooms/Alpha_20Hotel_20Kaghan_20Night_20view_td7lic.webp",
     expiryDate: "20 August 2026"
   },
   {
@@ -44,7 +44,7 @@ export const offers = [
     discount: "22% OFF",
     city: "Peshawar",
     description: "Experience the historic charm of Peshawar with boutique heritage hotels.",
-    img: "/images/offers/peshawar.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875113/rooms/exterior4_mzravc.jpg",
     expiryDate: "18 July 2026"
   },
   {
@@ -52,7 +52,7 @@ export const offers = [
     discount: "27% OFF",
     city: "Quetta",
     description: "Enjoy premium hospitality and scenic desert landscapes in Quetta.",
-    img: "/images/offers/quetta.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875173/rooms/quetta-exterior_jctbr8.jpg",
     expiryDate: "12 August 2026"
   },
   {
@@ -60,7 +60,7 @@ export const offers = [
     discount: "24% OFF",
     city: "Multan",
     description: "Explore the city of saints with comfortable hotels and cultural heritage tours.",
-    img: "/images/offers/multan.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875377/rooms/Hotel-one-tariq-road-multan_vh35dp.jpg",
     expiryDate: "05 August 2026"
   },
   {
@@ -68,7 +68,7 @@ export const offers = [
     discount: "32% OFF",
     city: "Faisalabad",
     description: "Perfect family stay packages with large rooms and dining discounts.",
-    img: "/images/offers/faisalabad.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875418/rooms/82073976_zanttf.jpg",
     expiryDate: "30 July 2026"
   },
   {
@@ -76,7 +76,7 @@ export const offers = [
     discount: "40% OFF",
     city: "Swat",
     description: "Stay in the Switzerland of Pakistan with breathtaking valley views.",
-    img: "/images/offers/swat.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875469/rooms/swat_efxpsy.jpg",
     expiryDate: "15 August 2026"
   },
   {
@@ -84,7 +84,7 @@ export const offers = [
     discount: "29% OFF",
     city: "Gilgit",
     description: "Premium hotels in Gilgit offering panoramic mountain views and luxury services.",
-    img: "/images/offers/gilgit.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875515/rooms/gilgt_scg8dq.jpg",
     expiryDate: "10 September 2026"
   },
   {
@@ -92,7 +92,7 @@ export const offers = [
     discount: "26% OFF",
     city: "Murree",
     description: "Cool weather and pine forests await in Murree’s best resorts.",
-    img: "/images/offers/murree.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875557/rooms/murree_ajz32b.jpg",
     expiryDate: "30 July 2026"
   },
   {
@@ -100,7 +100,7 @@ export const offers = [
     discount: "33% OFF",
     city: "Hunza",
     description: "Special couples package with luxury suites and mountain sunrise views.",
-    img: "/images/offers/hunza2.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875694/rooms/Hunza_Serena_xd1o5k.jpg",
     expiryDate: "20 August 2026"
   },
   {
@@ -108,7 +108,7 @@ export const offers = [
     discount: "31% OFF",
     city: "Skardu",
     description: "Stay near Shangrila Lake resorts with exclusive seasonal discounts.",
-    img: "/images/offers/skardu2.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875716/rooms/night-view_sa0qze.jpg",
     expiryDate: "25 August 2026"
   },
   {
@@ -116,7 +116,7 @@ export const offers = [
     discount: "23% OFF",
     city: "Chitral",
     description: "Adventure stays near Kalash Valley with cultural tours included.",
-    img: "/images/offers/chitral.jpg",
+    img: "https://res.cloudinary.com/dhjf7rok5/image/upload/v1774875835/rooms/maxresdefault_cqovcx.jpg",
     expiryDate: "05 September 2026"
   }
 ];
