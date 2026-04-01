76aff53e7cc4ad5ccca330c79fd206eae8f31ef6
Ayman-Kz25 <kamalakz786@gmail.com>
2026-04-02 02:50:15 +0500

    commit 150

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import { clerkClient, clerkMiddleware, getAuth, requireAuth } from "@clerk/express";
+import {
+  clerkMiddleware,
+} from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
@@ -11,8 +13,7 @@ import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import reviewRouter from "./routes/reviewRoutes.js";
 import offerRouter from "./routes/offerRoutes.js";
-import { stripeWebhooks } from "./controllers/StripeWebhooks.js";
-
+import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 const app = express();
 connectDB();
@@ -23,23 +24,24 @@ app.use(cors()); //Enable Cross-Origin Resource Sharing
 app.use(clerkMiddleware());
 
 //API to Listen to Stripe webhooks
-app.post("/api/stripe", express.raw({type: "application/json"}), stripeWebhooks);
+app.post(
+  "/api/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhooks,
+);
 
 //Middleware
 app.use(express.json());
 
 // API to listen to Clerk Webhooks
-app.use("/api/clerk", clerkWebhooks)
-
-
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
-app.use('/api/user', userRouter);
-app.use('/api/hotels', hotelRouter);
-app.use('/api/rooms', roomRouter);
-app.use('/api/bookings', bookingRouter);
-app.use('/api/review', reviewRouter);
-app.use('/api/offers', offerRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
+app.use("/api/review", reviewRouter);
+app.use("/api/offers", offerRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
